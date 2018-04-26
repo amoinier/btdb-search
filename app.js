13b2b68cb0433eef6300af360c72a21dcef03394
amoinier <amoinier@student.42.fr>
2018-04-26 22:27:44 +0200

    clean code + es6 + add options

--- app.js
+++ app.js
@@ -1,50 +1,58 @@
-/**
- * Criado por Adriel Mendes 27/10/2017
- */
-
 const thenfunc = require('q')
 const request = require('request')
 const cheerio = require('cheerio')
 const async = require('async')
 
-exports.search = function (searchStr, options) {
-  const defr = thenfunc.defer()
-  let result = []
-  let startPage = 1, endPage = 1, sort = ''
+const website_url = 'https://btdb.to/q/'
 
-  if (options) {
-    startPage = options.page_min || 1
-    endPage = options.page_max || 1
-    sort = options.sorts || ''
-  }
+exports.search = (searchStr, params) => {
+	const defr = thenfunc.defer()
+	let result = []
 
-  async.times(((endPage - startPage) + 1), (index, times_cb) => {
-    const option = {url: 'https://btdb.to/q/' + encodeURIComponent(searchStr) + '/' + (startPage + index) + '?sort=' + sort}
-	    request(option, function (err, resp, html) {
-	        let $ = cheerio.load(html)
-	        let elems = $('li[class=search-ret-item]'), count = elems.length
-	        elems.each(function () {
-	            let magnet = $(this).find('a[class=magnet]').attr('href')
-	            let name = $(this).find('.item-title a').attr('title')
-	            let size = $(this).find('.item-meta-info-value').eq(0).text()
-	            let popularity = $(this).find('.item-meta-info-value').eq(3).text()
-          let list_files = []
+	if (params) {
+		params.page_min = params.page_min || 1
+		params.page_max = params.page_max || 1
+		params.sorts = params.sorts || ''
+		params.timeout = (params.timeout && parseInt(params.timeout) == params.timeout ? params.timeout : 0)
+	}
 
-          let files = $(this).find('.file')
-          files.each((i, file) => {
-            list_files.push({
-              type: $(file).find('.file-icon').find('i').attr('class').replace('fa', '').replace('fa-', '').replace('-o', '').trim(),
-              name: $(file).find('.file-name').text().trim(),
-              size: $(file).find('.file-size').text().trim()
-            })
-          })
+	async.times(((params.page_max - params.page_min) + 1), (index, times_cb) => {
+		const option = {url: website_url + encodeURIComponent(searchStr) + '/' + (params.page_min + params.page_max) + '?sort=' + params.sorts}
+		if (params.timeout) {
+			option.timeout = params.timeout
+		}
+		request(option, (err, resp, html) => {
+			if (err || !html) {
+				return times_cb()
+			}
 
-	            result.push({magnet, name, size, popularity, list_files})
-	            if (!--count) { return times_cb() }
-	        })
-    })
-  }, (err, res) => {
-    return defr.resolve(result)
-  })
-  return defr.promise
+			let $ = cheerio.load(html)
+			let elems = $('li[class=search-ret-item]')
+			let count = elems.length
+
+			elems.each((i, elem) => {
+				let torrent = {
+					magnet: $(elem).find('a[class=magnet]').attr('href'),
+					name: $(elem).find('.item-title a').attr('title'),
+					size: $(elem).find('.item-meta-info-value').eq(0).text(),
+					popularity: $(elem).find('.item-meta-info-value').eq(3).text(),
+					list_files: []
+				}
+
+				$(elem).find('.file').each((i, file) => {
+					torrent.list_files.push({
+						type: $(file).find('.file-icon').find('i').attr('class').replace('fa', '').replace('fa-', '').replace('-o', '').trim(),
+						name: $(file).find('.file-name').text().trim(),
+						size: $(file).find('.file-size').text().trim()
+					})
+				})
+
+				result.push(torrent)
+				if (!--count) { return times_cb() }
+			})
+		})
+	}, (err, res) => {
+		return defr.resolve(result)
+	})
+	return defr.promise
 }
--- test/test.js
+++ test/test.js
@@ -1,2 +1,2 @@
 var btdb = require('../app.js');
-btdb.search('Ubuntu', {sorts: 'popular', page_min: 1, page_max: 5}).then(data => {console.log(JSON.stringify(data, null, 2));});
+btdb.search('Ubuntu', {sorts: 'popular', page_min: 3, page_max: 5, timeout: 0}).then(data => {console.log(JSON.stringify(data, null, 2));});
